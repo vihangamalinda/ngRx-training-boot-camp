830af513c50f0516a7343ad87516261740078237
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-03 11:15:44 -0800

    feat: Alternate way of defining reducers without using createReducer().

--- 01-starting-setup-standalone/src/app/store/counter.reducer.ts
+++ 01-starting-setup-standalone/src/app/store/counter.reducer.ts
@@ -4,13 +4,18 @@ import { decrement, increment } from "./counter.action";
 const initialState =0;
 
 // New version of declaring a reducers in ngRx
-export const counterReducer = createReducer(
-initialState,
-on(increment,(state,action)=>state+action.value),
-on(decrement,(state,action)=>state-action.value)
-);
+// export const counterReducer = createReducer(
+// initialState,
+// on(increment,(state,action)=>state+action.value),
+// on(decrement,(state,action)=>state-action.value)
+// );
 
 // Older way(version) of declaring a reducers in ngRx
-// export function counterReducer(state=initialState){
-//     return initialState;
-// }
+export function counterReducer(state=initialState,action:any){
+    if(action.type === '[Counter] Increment'){
+        return state+action.value;
+    }else if(action.type === '[Counter] Decrement'){
+        return state-action.value;
+    }
+    return state;
+}
