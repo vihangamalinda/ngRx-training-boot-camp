88feb4141c11f1099044f37c653d840be2b6203f
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-03 05:04:45 -0800

    feat:adding ngrx to standalone project

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+    providers: [provideStore()]
+});
