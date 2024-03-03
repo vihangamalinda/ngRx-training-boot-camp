8b112a6b557eb31c3de22572c7dcc1025153b0a4
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-03 10:31:49 -0800

    feat:Learning manipuilate state with a fixed values.(without props)

--- 01-starting-setup-standalone/src/app/counter-controls/counter-controls.component.ts
+++ 01-starting-setup-standalone/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { decrement, increment } from '../store/counter.action';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +11,13 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store:Store) {}
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment())
   }
 
   decrement() {
-    this.counterService.decrement();
+    this.store.dispatch(decrement())
   }
 }
--- 01-starting-setup-standalone/src/app/store/counter.reducer.ts
+++ 01-starting-setup-standalone/src/app/store/counter.reducer.ts
@@ -1,10 +1,13 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer,on } from "@ngrx/store";
+import { decrement, increment } from "./counter.action";
 
 const initialState =0;
 
 // New version of declaring a reducers in ngRx
 export const counterReducer = createReducer(
 initialState,
+on(increment,(state)=>state+1),
+on(decrement,(state)=>state-1)
 );
 
 // Older way(version) of declaring a reducers in ngRx
