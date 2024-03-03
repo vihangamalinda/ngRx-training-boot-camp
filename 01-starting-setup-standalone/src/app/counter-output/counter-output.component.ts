3c17ad0d9f2a3797e4cd2fa6219f9750c0d91d14
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-03 06:40:11 -0800

    feat: Read data from store.

--- 01-starting-setup-standalone/src/app/counter-output/counter-output.component.ts
+++ 01-starting-setup-standalone/src/app/counter-output/counter-output.component.ts
@@ -1,29 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { CounterService } from '../counter.service';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
   standalone: true,
+  imports:[AsyncPipe]
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter = 0;
-  counterServiceSub?: Subscription;
+export class CounterOutputComponent {
 
-  constructor(private counterService: CounterService) {}
+  count$:Observable<number>;
 
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
+  constructor(private store:Store<{counter:number}>) {
+    this.count$ =store.select('counter');
   }
 
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
-  }
 }
