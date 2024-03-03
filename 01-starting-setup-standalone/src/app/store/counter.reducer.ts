77a0772630c55fb48e4e5e56eba059952aeef86b
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-03 05:19:48 -0800

    info: Alternative approaches to declare reducers  in older versions of ngrx.

--- 01-starting-setup-standalone/src/app/store/counter.reducer.ts
+++ 01-starting-setup-standalone/src/app/store/counter.reducer.ts
@@ -2,7 +2,12 @@ import { createReducer } from "@ngrx/store";
 
 const initialState =0;
 
+// New version of declaring a reducers in ngRx
 export const counterReducer = createReducer(
 initialState,
-
 );
+
+// Older way(version) of declaring a reducers in ngRx
+// export function counterReducer(state=initialState){
+//     return initialState;
+// }
