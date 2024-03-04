d8bc0b15b64ac9a099dcbb0bb99b1a63176022b8
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-04 02:01:35 -0800

    feat: Adding ngrx effects dependency.

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -4,7 +4,8 @@ import { provideStore } from '@ngrx/store';
 
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore({counter:counterReducer})]
+    providers: [provideStore({ counter: counterReducer }), provideEffects()]
 });
