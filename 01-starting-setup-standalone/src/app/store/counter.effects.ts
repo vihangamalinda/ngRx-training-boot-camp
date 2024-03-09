327adadeed89b91a96a028c99264d5186e0899cf
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-09 04:01:08 -0800

    feat:Storing the latest counter value.

--- 01-starting-setup-standalone/src/app/store/counter.effects.ts
+++ 01-starting-setup-standalone/src/app/store/counter.effects.ts
@@ -1,17 +1,20 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.selectors";
 
 @Injectable()
 export class CounterEffects{
     saveCount = createEffect(()=>this.actions$.pipe(
         ofType(increment,decrement),
-        tap((action)=>{
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action,counter])=>{
             console.log(action);
-            localStorage.setItem('count',action.value.toString())
+            localStorage.setItem('count',counter.toString())
         })
     ),{dispatch:false});
 
-    constructor(private actions$:Actions){}
+    constructor(private actions$:Actions,private store:Store<{counter:number}>){}
 }
