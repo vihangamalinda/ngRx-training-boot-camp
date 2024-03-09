1582a5bd1cd4e7c64d832005e06e6367707a7bff
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-03-09 03:56:10 -0800

    feat:adding effect to add data inthe initial run.

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -5,7 +5,8 @@ import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducers';
 import { provideEffects } from '@ngrx/effects';
+import { CounterEffects } from './app/store/counter.effects';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore({ counter: counterReducer }), provideEffects()]
+    providers: [provideStore({ counter: counterReducer }), provideEffects([CounterEffects])]
 });
